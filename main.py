8e7cdd9953e8aa609d767b9a489da4c3dbdb959a
VinG12345 <vg122333@gmail.com>
2026-04-21 23:02:20 +0400

    part of game

--- main.py
+++ main.py
@@ -4,25 +4,29 @@ pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, S))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption('Игра Тир')
-icon = pygame.image.load('img/123.jpg')
+icon = pygame.image.load('img/target.jpg')
 pygame.display.set_icon(icon)
 
-target_image = pygame.image.load('img/target')
+target_image = pygame.image.load('img/target_2.jpg')
 target_width = 80
 target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
-color = (random.randint(0, 255)), random.randint(0, 255)
-
-
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(target_image, (target_x, target_y))
+    pygame.display.update()
 
 
 
